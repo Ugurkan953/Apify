48053adc70615388fcc5521a8195d3efb6293e41
Ugurkan <ugurkan953@gmail.com>
2015-03-11 15:35:15 +0100

    door ugur phpstorm

---
+++ includes/initialize.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Require all files that are needed for the website
+ */
+
+
+/*
+ * Create new classes
+ */
+
+
+
+/*
+ * Check for button clicks, forward to right functions inside of classes
+ */
